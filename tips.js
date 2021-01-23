2e4167c5f03c25b3998ca788c8d0788535b5a854
system@realstudiosonline.com <system@realstudiosonline.com>
2021-01-23 10:34:42 +0000

    update

--- tips.js
+++ tips.js
@@ -1,2 +1,10 @@
 //style console variable
 console.log('%c this_var', 'color: blue; font-size: 1.2rem' );
+
+
+//computer property names
+const foo ={isaac:'programmer', age:'16' }
+const girlfriend ={no:'girlfriend', why:'too poor' }
+const change ={million:'50', rich:'yes' }
+
+    console.log({foo, girlfriend,change})
